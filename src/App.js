fa9bbd63be255b1382df46ec99de097be573a6f7
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-03 18:06:57 -0300

    moved app components into WebsiteLayout, created routes in WebsiteLayout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
 import './styles/Library.css'
 import './App.css';
-import Header from './components/Header';
-import HeaderBar from './components/HeaderBar';
-import Footer from './components/Footer'
 import WebsiteLayout from './layouts/WebsiteLayout';
 
 function App() {
   return (
     <>
-      <Header />
-      <HeaderBar />
       <WebsiteLayout />
-      <Footer />
     </>
   );
 }
--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Home from '../pages/Home';
-import Productos from '../pages/Productos';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import HeaderBar from '../components/HeaderBar';
 import UnderConstruction from '../pages/UnderConstruction';
+import Productos from '../pages/Productos';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
 import Profile from '../pages/Profile';
 import Alquileres from '../pages/Alquileres';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Contacto from '../pages/Contacto';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default function WebsiteLayout(props) {
+export default function WebsiteLayout() {
     return (
         <>
             <BrowserRouter>
+                <Header />
+                <HeaderBar />
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='*' element={<UnderConstruction />} />
@@ -23,6 +28,7 @@ export default function WebsiteLayout(props) {
                     <Route path='/auth/signin' element={<SignIn />} />
                     <Route path='/auth/signup' element={<SignUp />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </>
     )
