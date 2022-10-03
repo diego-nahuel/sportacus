eea6cc0b5c2a8b559b701ca5ce7e950083562a9c
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-03 18:13:30 -0300

    update footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Footer.css'
+import { Link as LinkRouter } from 'react-router-dom'
 
 export default function Footer() {
+  function ScrollTop(){
+    window.scrollTo({top:0, behavior: 'smooth'})
+  }
   return (
-    <div>Footer</div>
+    <footer className='Footer'>
+      <LinkRouter to='/' className='Inicio-link'> Inicio </LinkRouter>
+      <LinkRouter to='/contacto' className='Contacto-link'> Contacto </LinkRouter>
+    </footer>
   )
 }
