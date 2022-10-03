6293f088ee0622364dfaa4db2c8a7592ad47d223
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-03 18:13:15 -0300

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './styles/Library.css'
 import './App.css';
 import Header from './components/Header';
 import HeaderBar from './components/HeaderBar';
 import Footer from './components/Footer'
 import WebsiteLayout from './layouts/WebsiteLayout';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
+    <ScrollToTop/>
       <Header />
       <HeaderBar />
       <WebsiteLayout />
