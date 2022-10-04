e826dd697b9d70ddedf8e325715fe4954e736e5e
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-04 13:36:34 -0300

    created Comentarios and NuevoComentario components for Comunidad page

---
+++ src/components/Comentarios/Comentarios.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Comentarios() {
+    return (
+        <>
+            <div className='Contenedor-Comentario row'>
+                <div className='Usuario-Info col justify-start align-center text-center'>
+                    <img className='Usuario-IMG' src='https://static2.abc.es/media/play/2020/09/29/avatar-kE4H--1024x512@abc.jpeg' />
+                    <h4>User</h4>
+                </div>
+                <p className='Comentario'>Co mmentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmenentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmenentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmen</p>
+            </div>
+        </>
+    )
+}
