981078658357307b2454b565630b0ea1f7b1e29c
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-04 13:46:16 -0300

    basic structure for Contacto

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Contacto() {
   return (
-    <div>Contacto</div>
+    <>
+      <div className='col flex-center main-bg main-height text-light'>
+        <div className='row'>
+          <div>Info</div>
+
+          <div>Form</div>
+        </div>
+
+        <div>Mapa</div>
+      </div>
+    </>
   )
 }
