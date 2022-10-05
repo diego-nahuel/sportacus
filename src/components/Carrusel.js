c20f864a1136d65c1807944bb78407f90a662c0f
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-05 14:09:59 -0300

    update carousel

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -13,17 +13,20 @@ export default function Carrusel() {
         autoplay={{delay:3000, disableOnInteraction:false}}
         navigation={true} 
         modules={[Autoplay, Pagination, Navigation]}>
-          <SwiperSlide>
+          {/* <SwiperSlide>
             <img className='Carousel-img' src= 'https://cdn.pixabay.com/photo/2020/06/16/20/07/football-5307071_960_720.jpg' alt='cancha de futbol'/>
+          </SwiperSlide> */}
+          <SwiperSlide>
+            <img className='Carousel-img' src= 'https://img.freepik.com/fotos-premium/balon-futbol-realista-o-patron-basico-futbol-campo-hierba-verde-luz-solar-sol_387624-128.jpg?w=1800' alt='pelota de fútbol'/>
           </SwiperSlide>
           <SwiperSlide>
-            <img className='Carousel-img' src= 'https://img.freepik.com/fotos-premium/balon-futbol-realista-o-patron-basico-futbol-campo-hierba-verde-luz-solar-sol_387624-128.jpg?w=1800' alt='pelota de futbol'/>
+            <img className='Carousel-img' src= 'https://www.ricoh.es/media/Tennis-Banner_tcm77-13634.jpg' alt='cancha de tenis'/>
           </SwiperSlide>
           <SwiperSlide>
-            <img className='Carousel-img' src= 'https://www.ricoh.es/media/Tennis-Banner_tcm77-13634.jpg' alt='raqueta de tenis'/>
+            <img className='Carousel-img' src= '/basquetball.jpg' alt='cancha de básquet'/>
           </SwiperSlide>
           <SwiperSlide>
-            <img className='Carousel-img' src= 'https://integralspor.com/uploads/blog/detail/1618e792d4b1254221.jpg' alt='cancha de tenis'/>
+            <img className='Carousel-img' src= 'https://integralspor.com/uploads/facility/original/voleybol-sahalari-27752.jpg' alt='cancha de voley'/>
           </SwiperSlide>
         </Swiper>
 
