77ae67f589b08b21f91acb076699615b0f7ded01
juanrivarola <jeryflr@gmail.com>
2022-10-06 17:33:32 -0300

    añadi products

--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
 import { authAPI } from "./features/authAPI";
+import { productsApi } from "./features/productsAPI";
+
 
 export default configureStore ({
     reducer: {
         user: userReducer,
-        [authAPI.reducerPath] : authAPI.reducer
+        [authAPI.reducerPath] : authAPI.reducer,
+
+        products: productsApi,
+        [productsApi.reducerPath]: productsApi.reducer
+
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(authAPI.middleware)
