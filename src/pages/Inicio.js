c23b73c16583a2e5c6406957e308d49a9cfeb297
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-06 19:52:39 -0300

    add key

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -7,7 +7,7 @@ const sports = [
   { name: 'Sport2', photo: 'https://images.unsplash.com/photo-1546608235-3310a2494cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=638&q=80', description: 'Sport 2' },
   { name: 'Sport3', photo: 'https://images.unsplash.com/photo-1546608235-3310a2494cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=638&q=80', description: 'Sport 3' },
 ]
-const sportList = (sport) => <div className='Card'>
+const sportList = (sport, index) => <div className='Card' key={index}>
   <h4 className='text-center'>{sport.name}</h4>
   <img className='IMG-Card' src={sport.photo} />
   <p>{sport.description}</p>
@@ -18,7 +18,7 @@ const products = [
   { name: 'Sport2', photo: 'https://images.unsplash.com/photo-1546608235-3310a2494cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=638&q=80', description: 'Sport 2' },
   { name: 'Sport3', photo: 'https://images.unsplash.com/photo-1546608235-3310a2494cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=638&q=80', description: 'Sport 3' },
 ]
-const productList = (product) => <div className='Card'>
+const productList = (product, index) => <div className='Card' key={index}>
   <h4 className='text-center'>{product.name}</h4>
   <img className='IMG-Card' src={product.photo} />
   <p>{product.description}</p>
