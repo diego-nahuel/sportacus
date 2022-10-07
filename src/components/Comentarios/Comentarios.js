c14dfc4709ee5a2ab11d8920a545aa92ea0fea6c
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-06 23:37:56 -0300

    Fixed classes and styles to be responsive again

--- src/components/Comentarios/Comentarios.js
+++ src/components/Comentarios/Comentarios.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default function Comentarios() {
+
+    const user = {
+        name: 'Lionel',
+    }
     return (
         <>
             <div className='Contenedor-Comentario row'>
-                <div className='Usuario-Info col align-start'>
+                <div className='Usuario-Info col align-start pad-10 gap-5'>
                     <img className='Usuario-IMG' src='https://static2.abc.es/media/play/2020/09/29/avatar-kE4H--1024x512@abc.jpeg' />
-                    <h4 className='align-center'>User</h4>
+                    <h4 className='align-center w-normal'>{user.name}</h4>
                 </div>
+                <div className='ydivider-dark transparent-25'></div>
                 <p className='Comentario'>Co mmentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmenentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmenentCo mmentCommentCommentCommentComment CommentCommentCommentdawdntComm mmen</p>
             </div>
         </>
