1dbfe1b5359a6794cd3df4853fb235d33a3fda5a
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-11 18:47:43 -0300

    Created styles for 404 page

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -3,9 +3,15 @@ import React from 'react'
 export default function UnderConstruction() {
     return (
         <>
-            <div className='Main-BG flex-center'>
-                <h2>404 Under Construction</h2>
-            </div>
+            <main className='main-bg text-light justify-center'>
+                <div className='container-width justify-center bg blur main-height pad-10'>
+                    <div className='w100'>
+                        <h2 className='br3 text-center ypad-5 xpad-10 font-xxl'>
+                            <span className='Slogan01'>404 </span>-<span className='Slogan-Red'> Under Construction</span>
+                        </h2>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
