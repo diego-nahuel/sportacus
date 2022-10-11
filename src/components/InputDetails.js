122d41677268ea0c9689e748a61b290ee374993a
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-11 13:54:42 -0300

    adjusted title position

--- src/components/InputDetails.js
+++ src/components/InputDetails.js
@@ -7,7 +7,7 @@ export default function InputDetails(props) {
             <main className='main-bg main-height text-light justify-center'>
                 <div className='container-width justify-center bg blur col gap-15 ypad-10'>
                     <div className="br3 bg-dark col align-center tpad-5 gap-5">
-                        <h2>{field.name}</h2>
+                        <h2 className="xpad-10 text-center">{field.name}</h2>
                         <img className="w100" src={field.image} />
                         <div className="font-16 xpad-10 align-start col w100">
                             <span>{field.description}</span>
