1a56fbfe32dbd818fedc3c063e18d51309d50032
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-12 14:33:43 -0300

    implemented minimum height to container

--- src/pages/Comunidad.js
+++ src/pages/Comunidad.js
@@ -6,10 +6,10 @@ import NuevoComentario from '../components/Comentarios/NuevoComentario'
 function Comunidad() {
 
   const commentsArray = [
-    { name: 'Ignacio', comment: 'Holi', userPhoto: 'https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png' },
-    { name: 'Jere', comment: 'Holi', userPhoto: 'https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png' },
+    { name: 'Ignacio', comment: 'Holi', userPhoto: 'https://media.distractify.com/brand-img/payNR5Jfb/0x0/is-korra-the-last-avatar-1595380417772.png' },
+    { name: 'Jere', comment: 'Holi', userPhoto: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2022/03/avatar-facebook-2632445.jpg?itok=humq0Qgg' },
     { name: 'Diego', comment: 'Holi', userPhoto: 'https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png' },
-    { name: 'Juan', comment: 'Holi', userPhoto: 'https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png' }
+    { name: 'Juan', comment: 'Holi', userPhoto: 'https://www.clarin.com/img/2020/08/16/avatar-the-last-airbender-foto___pck4_8liS_1256x620__2.jpg#1597613994196' }
   ]
   const commentsFunction = (comment) =>
     <>
