d4d2ed2cd8b2f6a52318bc6d8a668fa92a813f2b
juanrivarola <jeryflr@gmail.com>
2022-10-12 13:47:03 -0300

    añadi comentarios

--- src/pages/FieldsDetails.js
+++ src/pages/FieldsDetails.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import apiUrl from "../API";
 import { useState, useEffect } from "react";
 import InputDetails from "../components/InputDetails";
-
+import { useGetFromFieldQuery } from "../features/commentAPI";
+import Comentarios from "../components/Comentarios/Comentarios";
 
 export default function FieldsDetails() {
     const id = useParams()
@@ -12,7 +13,10 @@ export default function FieldsDetails() {
         axios.get(apiUrl + '/fields/' + id.id)
             .then(response => setField(response.data.response))
     }, [])
-    console.log(field)
 
-    return (<InputDetails data={field} />)
-}
+    return (
+    <>
+    <InputDetails data={field} />
+    <Comentarios/>
+    </>
+)}
