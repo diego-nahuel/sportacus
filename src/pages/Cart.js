6009df58cb7ef2fc3cade4f583a85eefa48b0c6a
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-12 19:36:10 -0300

    Create cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ShoppingCart from '../components/ShoppingCart'
+import '../styles/Components.css'
+
+function Cart() {
+  return (
+    <div>
+        <ShoppingCart/>
+    </div>
+  )
+}
+
+export default Cart
