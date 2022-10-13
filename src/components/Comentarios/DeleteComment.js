a4a16e9d6e76a28788a9aeebdf96d3f511b0bb32
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-13 14:37:18 -0300

    changed UpdateComment and DeleteComment styles

--- src/components/Comentarios/DeleteComment.js
+++ src/components/Comentarios/DeleteComment.js
@@ -1,18 +1,18 @@
-import {useDeleteCommentMutation} from '../../features/commentAPI'
+import { useDeleteCommentMutation } from '../../features/commentAPI'
 import '../../styles/Comment.css'
 
-export default function DeleteComment(props){
+export default function DeleteComment(props) {
     const [deleteComment] = useDeleteCommentMutation()
 
     const id = props.data
 
-    const handleDelete = () =>{
+    const handleDelete = () => {
         deleteComment(id)
     }
 
-    return(
-        <div>
-            <button className='deleteComment' type="button" onClick={handleDelete}>Eliminar</button>
-        </div>
+    return (
+        <>
+            <button className='br3-br br-lightblue transition form-padding bg-dark text-light' type="button" onClick={handleDelete}>Eliminar</button>
+        </>
     )
 }
