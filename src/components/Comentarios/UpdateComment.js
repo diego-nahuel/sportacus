480bd4e22b6a706f9be1a11070c0802db92b4cf6
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-13 19:21:32 -0300

    adjusted styles

--- src/components/Comentarios/UpdateComment.js
+++ src/components/Comentarios/UpdateComment.js
@@ -1,19 +1,20 @@
-import {useEditCommentMutation} from '../../features/commentAPI'
+import { useEditCommentMutation } from '../../features/commentAPI'
 import '../../styles/Comment.css'
+import styles from '../Styles'
 
-export default function EditComent(props){
+export default function EditComent(props) {
     const [editComment] = useEditCommentMutation()
 
     const id = props.id
     const data = props.data
 
     const handleEdit = () => {
-        editComment({id, data})
+        editComment({ id, data })
     }
 
-    return(
-        <div>
-            <button className='updateComment' type="submit" onClick={handleEdit}>Editar</button>
-        </div>
+    return (
+        <>
+            <button className={styles.editCommentButton} type="submit" onClick={handleEdit}>Editar</button>
+        </>
     )
 }
--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import '../styles/UserForm.css'
 import '../styles/Comunidad.css'
+import styles from '../components/Styles'
 
 export default function Profile() {
 
   const user = {
-    name: 'Lionel',
-    lastName: 'Messi',
-    mail: 'messilio@gmail.com',
-    class: 'w-normal bpad-10'
+    name: 'Ignacio',
+    mail: 'ignaciogiusti94@gma il.com'
   }
 
   const formUserData = [
     { name: 'text', type: 'name', placeholder: 'Nombre' },
-    { name: 'text', type: 'lastName', placeholder: 'Apellido' },
     { name: 'url', type: 'photo', placeholder: 'Avatar' }
   ]
   const userForm = (input) => <input type={input.type} name={input.name} className='br3 form-padding' placeholder={input.placeholder} /* onChange={handleInput} */></input>
-  
+
   return (
     <>
-      <main className='main-bg main-height justify-center'>
+      <main className='main-bg justify-center'>
         <div className="bg main-height blur container-width ypad-30 col align-center justify-start gap-30">
 
-          <h2 className='text-light'>Hola {user.name}, bienvenido a tu perfil!</h2>
+          <h2 className={styles.h2Profile}>Hola {user.name}, bienvenido a tu perfil!</h2>
 
           <div className='User-Info justify-center bg-dark br3 row gap-15 pad-15'>
-            <img src='https://wallpapercave.com/wp/wp4793571.jpg' alt='User Avatar' className='Usuario-Avatar justify-center' />
+            <img src='http://www.cmtv.com.ar/imagenes_artistas/2371.jpg?Porta' alt='User Avatar' className='Usuario-Avatar justify-center' />
 
             <div className='X-Profile-Divider transparent-25'></div>
             <div className='Y-Profile-Divider transparent-25'></div>
 
-            <dl className='text-light w-bold w100'>
+            <dl className='text-light w-bold'>
               <dt>Nombre: </dt>
-              <dd className={user.class}>{user.name}</dd>
-              <dt>Apellido: </dt>
-              <dd className={user.class}>{user.lastName}</dd>
+              <dd className={styles.infoProfile}>{user.name}</dd>
               <dt>Mail: </dt>
-              <dd className={user.class}>{user.mail}</dd>
+              <dd className={styles.infoProfile}>{user.mail}</dd>
             </dl>
+
           </div>
 
           <form className='Form-Container w50 bg-dark pad-15 form-width br3 gap-15 col'>
