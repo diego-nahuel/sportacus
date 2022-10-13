07ed8a751762258b0ca1af59fac85d6678ef7e87
juanrivarola <jeryflr@gmail.com>
2022-10-13 17:38:16 -0300

    añadi admin links

--- src/components/Links/PagesAdmin.js
+++ src/components/Links/PagesAdmin.js
@@ -1,8 +1,10 @@
-const pages = [
+const pagesAdmin = [
     { name: 'Home', to: '/' },
     { name: 'Canchas', to: '/canchas' },
     { name: 'Productos', to: '/productos' },
     { name: 'Comunidad', to: '/comunidad' },
     { name: 'Contacto', to: '/contacto' },
+    { name:"Agregar Cancha", to:'/nuevacancha'},
+    {name: "Agregar Producto", to:'/nuevoproducto'}
 ]
-export default pages;
+export default pagesAdmin;
