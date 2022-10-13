f7c0dd0a4bd07e9b5fe2b19bfeaf6c7fd3a6eea7
juanrivarola <jeryflr@gmail.com>
2022-10-13 20:45:00 -0300

    oculto paginas

--- src/components/Links/Pages.js
+++ src/components/Links/Pages.js
@@ -4,7 +4,5 @@ const pages = [
     { name: 'Productos', to: '/productos' },
     { name: 'Comunidad', to: '/comunidad' },
     { name: 'Contacto', to: '/contacto' },
-    { name: "Agregar Cancha", to: '/nuevacancha' },
-    { name: "Agregar Producto", to: '/nuevoproducto' }
 ]
 export default pages;
