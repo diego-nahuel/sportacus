aca4e98f3238c4ac49808a006bcf91b973d6e634
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-10-13 16:58:18 -0300

    change reducer

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,49 +1,42 @@
 import React, { useReducer } from 'react'
-import { shoppingInitialState, shoppingReducer } from '../reducers/ShoppingReducer'
+import { CLEAR_CART, REMOVE_ALL_FROM_CART, REMOVE_ONE_FROM_CART, shoppingInitialState, shoppingReducer } from '../reducers/ShoppingReducer'
 import ProductItem from './ProductItem'
 import '../styles/Components.css'
 import CartItem from './CartItem'
 import { TYPES } from '../actions/ShoppingAction'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 const ShoppingCart = () => {
-    const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)
-    const {products, cart} = state
+  const dispatch = useDispatch();
+    const cart = useSelector(state => state.shoppingReducer.cart)
+  console.log(cart);
 
-    const addToCart = (id) => {
-      //console.log(id);
-      dispatch({
-        type: TYPES.ADD_TO_CART,
-        payload: id
-      })
+    if(cart){
+      console.log(cart)
     }
+
 
     const delFromCart = (id,all=false) => {
       if(all){
-        dispatch({
-          type:TYPES.REMOVE_ALL_FROM_CART,
-          payload:id
-        })
+        dispatch(REMOVE_ALL_FROM_CART(id))
       } else {
-        dispatch({
-          type:TYPES.REMOVE_ONE_FROM_CART,
-          payload:id
-        })
+        dispatch(REMOVE_ONE_FROM_CART(id))
       }
     }
 
     const clearCart = () => {
-      dispatch({type: TYPES.CLEAR_CART})
+      dispatch(CLEAR_CART())
     }
 
   return (
     <>
     <h2>Carrito de compras</h2>
     <h3>Productos</h3>
-    <article className='card-container justify-center gap-30'>
+    {/* <article className='card-container justify-center gap-30'>
         {products.map((product) => <ProductItem key={product._id} data={product} addToCart={addToCart} />)}
     </article>
-    <h3>Carrito</h3>
+    <h3>Carrito</h3> */}
     <article className='box'>
       <button onClick={clearCart}>Limpiar Carrito</button>
       {
