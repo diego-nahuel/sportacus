199dd220fa60e53a1c87a2d683d5ec5030f7ceb1
ignaciogiusti <nachogiusti94@gmail.com>
2022-10-14 05:41:21 -0300

    changed images

--- src/actions/ProductList.js
+++ src/actions/ProductList.js
@@ -1,25 +1,25 @@
 const products = [
     {
         name: 'Bolsa de Boxeo',
-        photo: 'https://http2.mlstatic.com/D_NQ_NP_2X_677583-MLA51695843968_092022-F.webp',
+        photo: 'https://i.imgur.com/SnrAXsf.png',
         sport: 'Boxeo',
         stock: 5
     },
     {
         name: 'Raqueta De Tenis',
-        photo: 'https://http2.mlstatic.com/D_NQ_NP_2X_923725-MLA47934828603_102021-F.webp',
+        photo: 'https://i.imgur.com/Vq8C6jd.png',
         sport: 'Futbol',
         stock: 6
     },
     {
         name: 'Guantes De Boxeo',
-        photo: 'https://http2.mlstatic.com/D_NQ_NP_2X_973883-MLA50135341528_052022-F.webp',
+        photo: 'https://cdn.shopify.com/s/files/1/0066/7274/4499/products/Grijs_620x.png?v=1598927703',
         sport: 'Boxeo',
         stock: 4
     },
     {
         name: 'Name 3',
-        photo: 'https://http2.mlstatic.com/D_NQ_NP_2X_822411-MLA47196102185_082021-F.webp',
+        photo: 'https://i.imgur.com/V6e9MXW.png',
         sport: 'Natacion',
         stock: 4
     }
